00fbc314527853d1825e8ca6a9bed17a5dd59b57
AgusPk <agustinperkins@hotmail.com>
2024-10-18 12:55:32 -0300

    added mongodb config

---
+++ src/models/transfer.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const transferSchema = new mongoose.Schema({
+  from: {
+    type: String,
+    required: true,
+  },
+  to: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  cardId: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.models.Transfer ||
+  mongoose.model('Transfer', transferSchema);
