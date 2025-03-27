be003140dbd5476191fd672095c4fcb3a98cfd8e
AgusPk <agustinperkins@hotmail.com>
2025-03-27 11:34:09 -0300

    fix linting test

--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -1,45 +1,35 @@
 "use client"
 
-import type { ReactNode } from "react"
+import { ReactNode } from "react"
 import Image from "next/image"
 import type { Card as CardAPI } from "pokemon-tcg-sdk-typescript/dist/sdk"
-
 import type { Transfer } from "@/types/types"
-
 import styles from "./styles.module.css"
 
 type Props = {
-  card: CardAPI | Transfer
-  children: ReactNode
-  cardType: "card" | "transfer"
+	card: CardAPI | Transfer;
+	children: ReactNode;
+	cardType?: "card" | "transfer";
 }
 
 export default function Card({ card, children, cardType = "card" }: Props) {
-  let image
-  switch (cardType) {
-    case "card":
-      image = "images" in card ? card.images?.small : ""
-      break
-    case "transfer":
-      image = "cardImage" in card ? card.cardImage : ""
-      break
-    default:
-      image = ""
-  }
+	const image = cardType === "card" 
+		? "images" in card ? card.images?.small : ""
+		: "cardImage" in card ? card.cardImage : ""
 
-  return (
-    <div className={styles.container}>
-      {image && (
-        <Image
-          className={styles.image}
-          src={image || "/placeholder.svg"}
-          width={150}
-          height={200}
-          alt={card.name || ""}
-        />
-      )}
-      <div className={styles.cardContent}>{children}</div>
-    </div>
-  )
+	return (
+		<div className={styles.container}>
+			{image && (
+				<Image
+					className={styles.image}
+					src={image || "/placeholder.svg"}
+					width={150}
+					height={200}
+					alt={card.name || ""}
+				/>
+			)}
+			<div className={styles.cardContent}>{children}</div>
+		</div>
+	)
 }
 
